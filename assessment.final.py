64619e1bae4bfcea2316b86946a854d399a203e0
Taksh barot <takshbarot248@gmail.com>
2026-01-20 11:15:44 +0530

    Add files via upload

--- assessment.final.py
+++ assessment.final.py
@@ -1,42 +1,42 @@
-# tasks = []
+tasks = []
 
-# while True:
-#     print("\n--- TO-DO LIST MENU ---")
-#     print("1. Add Task")
-#     print("2. View Tasks")
-#     print("3. Delete Task")
-#     print("4. Exit")
+while True:
+    print("\n--- TO-DO LIST MENU ---")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Delete Task")
+    print("4. Exit")
 
-#     choice = input("Enter your choice: ")
-#     if choice == "1":
-#         task = input("Enter task: ")
-#         tasks.append(task)
-#         print("Task added successfully!")
-#     elif choice == "2":
-#         if not tasks:
-#             print("No tasks available.")
-    #     else:
-    #         print("\nYour Tasks:")
-    #         for i, task in enumerate(tasks, start=1):
-    #             print(f"{i}. {task}")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        task = input("Enter task: ")
+        tasks.append(task)
+        print("Task added successfully!")
+    elif choice == "2":
+        if not tasks:
+            print("No tasks available.")
+        else:
+            print("\nYour Tasks:")
+            for i, task in enumerate(tasks, start=1):
+                print(f"{i}. {task}")
 
-    # elif choice == "3":
-    #     if not tasks:
-    #         print("No tasks to delete.")
-    #     else:
-    #         num = int(input("Enter task number to delete: "))
-    #         if 1 <= num <= len(tasks):
-    #             removed = tasks.pop(num - 1)
-    #             print(f"Deleted task: {removed}")
-    #         else:
-    #             print("Invalid task number.")
+    elif choice == "3":
+        if not tasks:
+            print("No tasks to delete.")
+        else:
+            num = int(input("Enter task number to delete: "))
+            if 1 <= num <= len(tasks):
+                removed = tasks.pop(num - 1)
+                print(f"Deleted task: {removed}")
+            else:
+                print("Invalid task number.")
 
-    # elif choice == "4":
-    #     print("Exiting To-Do List. Goodbye!")
-    #     break
+    elif choice == "4":
+        print("Exiting To-Do List. Goodbye!")
+        break
 
-    # else:
-    #     print("Invalid choice. Try again.")
+    else:
+        print("Invalid choice. Try again.")
 
 
 #> wheather app(api):
