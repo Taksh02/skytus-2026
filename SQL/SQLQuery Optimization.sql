88c02fa37368775d195e5012f6e2c5e335f03002
Taksh barot <takshbarot248@gmail.com>
2026-01-29 08:37:14 +0530

    Add files via upload

---
+++ SQL/SQLQuery Optimization.sql
@@ -0,0 +1,33 @@
+USE EcommerceDB;
+
+-- Add index to improve search on orders.customer_id
+create index orders_customer_id
+on orders(customer_id);
+
+-- Use explain to analyze a query
+
+select * from orders
+where customer_id = 1;
+
+-- Optimize a slow join query
+
+create index customers_city
+on customers(city);
+
+select c.name, o.amount
+from customers c
+join orders o
+on c.customer_id = o.customer_id
+where c.city = 'Surat';
+
+-- When index Should not be used
+select *
+from orders
+where order_date between '2025-01-01' and '2025-12-31';
+
+select *
+from customers
+where city in ('Surat', 'Vadodara');
+
+SELECT name
+FROM sys.tables;
